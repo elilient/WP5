06c7450fd0b8927f807309cb1e5f24663f501c1f
edgarlilienthal <edgar.lilienthal@khk.ee>
2019-01-16 10:39:21 +0200

    modified plugin `Very First Plugin` version 1.0 (after deactivation of `Very First Plugin` version 1.0)

--- wp-content/plugins/very-first-plugin/very-first-plugin.php
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -8,6 +8,6 @@
  * Author URI: http://edgarlilienthal.ikt.khk.ee/
  **/
  function dh_modify_read_more_link() {
-     return '<a class="more-link href="' . get_permalink() . '">Click to Read!</a>';
- }
- add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+ return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+}
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
